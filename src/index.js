af564ae1ee0a6e32f9a98c71f2e3428663a2e856
Aldrin Dominique Natividad <aldmnatividad@gmail.com>
2018-06-30 21:01:03 -0400

    Revert back to previous repo seriously fuck this shit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Header from './components/common/Header.js';
+import Sidebar from './components/common/Sidebar.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const App = () => {
+  return (
+    <div class="page-wrap">
+      <Sidebar/>
+      <section id="main">
+        <Header/>
+      </section>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
